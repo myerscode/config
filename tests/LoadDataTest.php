d163471306e95647389bb79e89b732c46e1b9a43
Fred Myerscough <oniice@gmail.com>
2024-05-16 18:31:33 +0100

    tests(core): added tests for string that reference another string with \T or \t
    Signed-off-by: Fred Myerscough <oniice@gmail.com>

--- tests/LoadDataTest.php
+++ tests/LoadDataTest.php
@@ -92,6 +92,8 @@ class LoadDataTest extends TestCase
             'single_quotes' => 'string with single quotes: \'Hello, World!\'',
             'unicode_escape' => "string with a Unicode encode: \\u00A9 (\u00A9)",
             'Windows Filepath' => "D:\a\acorn-framework\acorn-framework\Tests\Mocks\DemoApp",
+            'Reference \T' => "D:\a\acorn-framework\acorn-framework\Tests\Mocks\DemoApp",
+            'Reference \t' => "D:\a\acorn-framework\acorn-framework\\tests\mock\demoapp",
         ];
 
         foreach ($escapedCharacters as $escapedCharacter => $value) {
